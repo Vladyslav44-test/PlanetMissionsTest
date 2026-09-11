30681c20ddb251822b914f1c95cec56f59db37b0
Vladislav44-test <elertvelert@gmail.com>
2026-09-11 16:28:32 +0300

    Додав код в метод Main, який видає виключення DivideByZeroException, тому що у класу PlanetMission всі поля за замовчуванням дорівнюють нулю, а код MissionInfo намагається поділити число на нуль

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace PlanetMissionsTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(new Venus().MissionInfo());
+            Console.WriteLine(new Mars().MissionInfo());
+            Console.WriteLine(new PlanetMission().MissionInfo());
         }
     }
 }
