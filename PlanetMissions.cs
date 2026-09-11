cff7ba883e36e4b6c78aac40c9a0191b4ceb395d
Vladislav44-test <elertvelert@gmail.com>
2026-09-11 16:23:10 +0300

    Додав базовий клас PlanetMission та два його субкласи: Mars і Venus, конструктори яких ініціалізують свої поля кожен своїми значеннями

---
+++ PlanetMissions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetMissionsTest
+{
+    internal class PlanetMission
+    {
+        protected float fuelPerKm;
+        protected long kmPerHour;
+        protected long kmToPlanet;
+
+        public string MissionInfo()
+        {
+            long fuel = (long)(kmToPlanet * fuelPerKm);
+            long time = kmToPlanet / kmPerHour;
+            return $"We'll burn {fuel} units of fuel in {time} hours";
+        }
+    }
+
+    class Mars : PlanetMission
+    {
+        public Mars()
+        {
+            kmToPlanet = 92000000;
+            fuelPerKm = 1.73f;
+            kmPerHour = 37000;
+        }
+    }
+
+    class Venus : PlanetMission
+    {
+        public Venus()
+        {
+            kmToPlanet = 41000000;
+            fuelPerKm = 2.11f;
+            kmPerHour = 29500;
+        }
+    }
+}
